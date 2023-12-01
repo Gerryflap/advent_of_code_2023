c52cc310209d21f6cb1fffd754a19354c2f20d35
gerben <gerry2211@gmail.com>
2023-12-01 19:15:31 +0100

    Clippy and fmt teaching me how to code

--- src/days.rs
+++ src/days.rs
@@ -8,25 +8,27 @@ pub fn run_day(day: i64, example: bool) {
     let days: &mut HashMap<i64, fn(String, String) -> String> = &mut HashMap::new();
     days.insert(1, day_1::run);
 
-    let func = days.get(&day)
-        .expect(format!("The given day {day} cannot be found !").as_str());
+    let func = days
+        .get(&day)
+        .unwrap_or_else(|| panic!("The given day {day} cannot be found !"));
 
     println!("Result: ");
     if example {
-        println!("{}", func(read_contents_example(day, 1), read_contents_example(day, 2)));
+        println!(
+            "{}",
+            func(read_contents_example(day, 1), read_contents_example(day, 2))
+        );
     } else {
         println!("{}", func(read_contents_real(day), read_contents_real(day)));
     }
 }
 
-
 fn read_contents_real(day: i64) -> String {
     fs::read_to_string(format!("./inputs/day_{day}.txt"))
-        .expect(format!("Could not read file './inputs/day_{day}.txt'!").as_str())
+        .unwrap_or_else(|_| panic!("Could not read file './inputs/day_{day}.txt'!"))
 }
 
 fn read_contents_example(day: i64, question: i64) -> String {
     fs::read_to_string(format!("./inputs/day_{day}_example_{question}.txt"))
-        .unwrap_or("".to_owned())
-
+        .unwrap_or_else(|_| "".to_owned())
 }
--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -4,20 +4,20 @@ pub fn run(input_1: String, input_2: String) -> String {
 
 fn q1(input: String) -> String {
     let result: i64 = input
-    .lines()
-    .map(|s| s.to_owned())
-    .map(combine_last_and_first_num)
-    .sum();
+        .lines()
+        .map(|s| s.to_owned())
+        .map(combine_last_and_first_num)
+        .sum();
 
     format!("{}", result)
 }
 
 fn q2(input: String) -> String {
     let result: i64 = input
-    .lines()
-    .map(|s| s.to_owned())
-    .map(combine_last_and_first_num_q2)
-    .sum();
+        .lines()
+        .map(|s| s.to_owned())
+        .map(combine_last_and_first_num_q2)
+        .sum();
 
     format!("{}", result)
 }
@@ -36,22 +36,20 @@ fn combine_last_and_first_num(line: String) -> i64 {
         .expect("Number should be parsable, but is not")
 }
 
-
 fn combine_last_and_first_num_q2(line: String) -> i64 {
     let nums: &mut Vec<i64> = &mut Vec::new();
-    let dbg_line = &line;
+    let _dbg_line = &line;
     let mut remaining: &str = &line;
 
-    while remaining.len() > 0 {
-        let res = parse_next(&remaining);
-        if res.is_some() {
-            let (n, nxt) = res.unwrap();
+    while !remaining.is_empty() {
+        let res = parse_next(remaining);
+        if let Some((n, nxt)) = res {
             nums.push(n);
             remaining = nxt;
         } else {
             remaining = remaining.split_at(1).1;
         }
-    };
+    }
 
     let number = format!(
         "{}{}",
@@ -64,11 +62,12 @@ fn combine_last_and_first_num_q2(line: String) -> i64 {
 }
 
 fn parse_next(text: &str) -> Option<(i64, &str)> {
-    parse_num(text).or(parse_written_num(text))
+    parse_num(text).or_else(|| parse_written_num(text))
 }
 
 fn parse_num(text: &str) -> Option<(i64, &str)> {
-    text.chars().next()
+    text.chars()
+        .next()
         .and_then(|c| c.to_digit(10))
         .map(|n| (n as i64, text.split_at(1).1))
 }
@@ -84,12 +83,13 @@ fn parse_written_num(text: &str) -> Option<(i64, &str)> {
         ("seven", 7),
         ("eight", 8),
         ("nine", 9),
-    ].iter()
+    ]
+    .iter()
     .filter_map(|(p, n)| match_str(text, p, *n))
     .next()
 }
 
-fn match_str<'a>(text: &'a str , pattern: &str, n: i64) -> Option<(i64, &'a str)> {
+fn match_str<'a>(text: &'a str, pattern: &str, n: i64) -> Option<(i64, &'a str)> {
     if text.starts_with(pattern) {
         // If you want to consume them all: Some((n, text.split_at(pattern.len()).1))
         Some((n, text.split_at(1).1))
