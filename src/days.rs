77983522e484d77921375cd5369e58808e12d2fa
gerben <gerry2211@gmail.com>
2023-12-01 17:15:47 +0100

    fmt and clippy were not happy :3

--- src/days.rs
+++ src/days.rs
@@ -8,18 +8,19 @@ pub fn run_day(day: i64, example: bool) {
     let days: &mut HashMap<i64, fn(String) -> String> = &mut HashMap::new();
     days.insert(1, day_1::run);
 
-    let func = days.get(&day)
-        .expect(format!("The given day {day} cannot be found !").as_str());
+    let func = days
+        .get(&day)
+        .unwrap_or_else(|| panic!("The given day {day} cannot be found !"));
 
-    let path: &mut String = &mut format!("./inputs/day_{day}").to_owned();
+    let path: &mut String = &mut format!("./inputs/day_{day}");
     if example {
         path.push_str("_example");
     }
     path.push_str(".txt");
     let path: &String = path;
 
-    let contents = fs::read_to_string(path)
-                                .expect(format!("Could not read file {path}").as_str());
+    let contents =
+        fs::read_to_string(path).unwrap_or_else(|_| panic!("Could not read file {path}"));
     println!("Result: ");
     println!("{}", func(contents));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 mod days;
 
 fn main() {
-    let name: String = std::env::args().next().expect("Script has no name?! Not sure what sorcery is going on here :3");
+    let name: String = std::env::args()
+        .next()
+        .expect("Script has no name?! Not sure what sorcery is going on here :3");
 
-    let day: i64 = std::env::args().nth(1)
+    let day: i64 = std::env::args()
+        .nth(1)
         .and_then(|s| s.parse().ok())
-        .expect(format!("Usage: {name} <day> [--example]").as_str());
+        .unwrap_or_else(|| panic!("Usage: {name} <day> [--example]"));
 
-    let example: bool = std::env::args().nth(2)
+    let example: bool = std::env::args()
+        .nth(2)
         .filter(|s| s == "--example")
         .is_some();
 
-    
     days::run_day(day, example);
 }
