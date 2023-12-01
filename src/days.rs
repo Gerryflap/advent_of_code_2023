88f0fb411febbe118054826c39ddcec0f6abf0db
gerben <gerry2211@gmail.com>
2023-12-01 17:39:24 +0100

    Restructure and started thinking about q2

--- src/days.rs
+++ src/days.rs
@@ -5,22 +5,28 @@ pub mod common;
 mod day_1;
 
 pub fn run_day(day: i64, example: bool) {
-    let days: &mut HashMap<i64, fn(String) -> String> = &mut HashMap::new();
+    let days: &mut HashMap<i64, fn(String, String) -> String> = &mut HashMap::new();
     days.insert(1, day_1::run);
 
-    let func = days
-        .get(&day)
-        .unwrap_or_else(|| panic!("The given day {day} cannot be found !"));
+    let func = days.get(&day)
+        .expect(format!("The given day {day} cannot be found !").as_str());
 
-    let path: &mut String = &mut format!("./inputs/day_{day}");
-    if example {
-        path.push_str("_example");
-    }
-    path.push_str(".txt");
-    let path: &String = path;
-
-    let contents =
-        fs::read_to_string(path).unwrap_or_else(|_| panic!("Could not read file {path}"));
     println!("Result: ");
-    println!("{}", func(contents));
+    if example {
+        println!("{}", func(read_contents_example(day, 1), read_contents_example(day, 2)));
+    } else {
+        println!("{}", func(read_contents_real(day), read_contents_real(day)));
+    }
+}
+
+
+fn read_contents_real(day: i64) -> String {
+    fs::read_to_string(format!("./inputs/day_{day}.txt"))
+        .expect(format!("Could not read file './inputs/day_{day}.txt'!").as_str())
+}
+
+fn read_contents_example(day: i64, question: i64) -> String {
+    fs::read_to_string(format!("./inputs/day_{day}_example_{question}.txt"))
+        .unwrap_or("".to_owned())
+
 }
--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -1,14 +1,37 @@
-pub fn run(input: String) -> String {
-    let result: i64 = input
-        .lines()
-        .map(|s| s.to_owned())
-        .map(combine_last_and_first_num)
-        .sum();
+pub fn run(input_1: String, input_2: String) -> String {
+    format!("Q1: {}\nQ2: {}", q1(input_1), q1(input_2))
+}
 
-    format!("Q1: {}", result)
+fn q1(input: String) -> String {
+    let result: i64 = input
+    .lines()
+    .map(|s| s.to_owned())
+    .map(combine_last_and_first_num)
+    .sum();
+
+    format!("{}", result)
+}
+
+fn q2(input: String) -> String {
+    "TODO".to_owned()
 }
 
 fn combine_last_and_first_num(line: String) -> i64 {
+    let line_nums: &Vec<char> = &line.chars().filter(|c| c.is_ascii_digit()).collect();
+
+    let number = format!(
+        "{}{}",
+        line_nums.first().expect("No numbers in line!"),
+        line_nums.last().expect("No numbers in line!")
+    );
+
+    number
+        .parse()
+        .expect("Number should be parsable, but is not")
+}
+
+
+fn combine_last_and_first_num_q2(line: String) -> i64 {
     let line_nums: &Vec<char> = &line.chars().filter(|c| c.is_ascii_digit()).collect();
 
     let number = format!(
