10bce701b54542f4d8c4c84e7bf0309ca5e58026
gerryflap <gerry2211@gmail.com>
2023-12-03 20:39:23 +0100

    fmt and clippy

--- src/days/day_2.rs
+++ src/days/day_2.rs
@@ -1,11 +1,12 @@
 use regex::Regex;
 
-pub fn run(input_1: String, input_2: String) -> String {
+pub fn run(input_1: String, _input_2: String) -> String {
     q1(input_1)
 }
 
 fn q1(inp: String) -> String {
-    let result: i64 = inp.lines()
+    let result: i64 = inp
+        .lines()
         .into_iter()
         .map(parse_game)
         .filter(|(_id, possible)| *possible)
@@ -17,20 +18,21 @@ fn q1(inp: String) -> String {
 fn parse_game(line: &str) -> (i64, bool) {
     let re = Regex::new(r"Game (?<game>\d+):(?<list>.*)").unwrap();
     let Some(caps) = re.captures(line) else {panic!("Cannot parse line {line}")};
-    let game: i64 = caps["game"].to_owned()
-                        .parse()
-                        .expect("There should be a game ID!");
+    let game: i64 = caps["game"]
+        .to_owned()
+        .parse()
+        .expect("There should be a game ID!");
     let possible: bool = is_game_possible(&caps["list"]);
     (game, possible)
 }
 
 fn is_game_possible(list: &str) -> bool {
-    let subsets = list.split(";");
+    let subsets = list.split(';');
     subsets.map(is_subset_possible).all(|v| v)
 }
 
 fn is_subset_possible(subset: &str) -> bool {
-    subset.split(",").map(is_color_possible).all(|v| v)
+    subset.split(',').map(is_color_possible).all(|v| v)
 }
 
 fn is_color_possible(color: &str) -> bool {
@@ -42,6 +44,6 @@ fn is_color_possible(color: &str) -> bool {
         "red" => count <= 12,
         "green" => count <= 13,
         "blue" => count <= 14,
-        _ => false
+        _ => false,
     }
 }
